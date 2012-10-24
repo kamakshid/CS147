6f6fd794608082bd33799c18e32161c18ae37389
Kamakshi <kduvvuru@gmail.com>
2012-10-24 14:30:16 -0700

    msg

--- week3/index.php
+++ week3/index.php
@@ -18,15 +18,16 @@
 		<table>
 		<?php
 		include("config.php");
-		$query = "";
+		$query = "Select * from books";
 		
 		// Don’t be intimidated by the following lines. You can pretty much
 		// always copy and paste these because they’re sort of like
 		// functional bits that never change.
 		$result = mysql_query($query);
 		while ($row = mysql_fetch_assoc($result)) {
-			echo "<p>".$row["title"]."</p>";
-		}
+			echo "<p>".$row["title"]." by ".$row["author"]."</p>";
+                        echo "<img src='".$row["image"]."' />";
+                }
 		?>
 		</table>
 		
